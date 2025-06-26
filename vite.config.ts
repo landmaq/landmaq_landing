82bc309652e770309261cf794b47f24ac435313f
landmaq <landmaq2025@gmail.com>
2025-06-26 15:59:48 +0100

    hot fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/landmaq-landing/",
+  base: "/landmaq_landing/",
   server: {
     host: "::",
     port: 8080,
